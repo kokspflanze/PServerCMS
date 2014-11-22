967db0a538ba6fb40987fb6dc7798ae700e16313
kokspflanze <kokspflanze@gmx.de>
2014-11-22 15:34:37 +0100

    init

--- src/PServerCMS/Module.php
+++ src/PServerCMS/Module.php
@@ -18,14 +18,14 @@ class Module {
 	}
 
 	public function getConfig() {
-		return include __DIR__ . '/config/module.config.php';
+		return include __DIR__ . '/../../config/module.config.php';
 	}
 
 	public function getAutoloaderConfig() {
 		return array(
 			'Zend\Loader\StandardAutoloader' => array(
 				'namespaces' => array(
-					__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+					__NAMESPACE__ => __DIR__ . '/../../src/' . __NAMESPACE__,
 				),
 			),
 		);
