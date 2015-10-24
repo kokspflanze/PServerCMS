b672562bd2f9811a177c061e332f1c14e221d52f
KoKsPfLaNzE <kokspflanze@gmx.de>
2015-10-24 22:06:28 +0200

    fix in set currentPlayer

--- src/PServerCMS/Service/PlayerHistory.php
+++ src/PServerCMS/Service/PlayerHistory.php
@@ -6,17 +6,16 @@ use PServerCMS\Keys\Caching;
 
 class PlayerHistory extends InvokableBase
 {
-
     /**
      * @return int
      */
     public function getCurrentPlayer()
     {
-        $currentPlayer = $this->getCachingHelperService()->getItem( Caching::PLAYER_HISTORY, function () {
+        $currentPlayer = $this->getCachingHelperService()->getItem(Caching::PLAYER_HISTORY, function () {
             /** @var \PServerCMS\Entity\Repository\PlayerHistory $repository */
-            $repository = $this->getEntityManager()->getRepository( $this->getEntityOptions()->getPlayerHistory() );
+            $repository = $this->getEntityManager()->getRepository($this->getEntityOptions()->getPlayerHistory());
             return $repository->getCurrentPlayer();
-        } );
+        });
 
         return $currentPlayer;
     }
@@ -24,21 +23,22 @@ class PlayerHistory extends InvokableBase
     /**
      * read from GameBackend the current player [or] as param and save them in database
      *
-     * @param int $player
+     * @param int $extraPlayer
      */
-    public function setCurrentPlayer( $player = 0 )
+    public function setCurrentPlayer($extraPlayer = 0)
     {
-        if (!$player) {
-            $player = $this->getGameBackendService()->getCurrentPlayerNumber();
+        $player = $this->getGameBackendService()->getCurrentPlayerNumber();
+        if ($player > 0) {
+            $player += $extraPlayer;
         }
 
         $class = $this->getEntityOptions()->getPlayerHistory();
         /** @var \PServerCMS\Entity\PlayerHistory $playerHistory */
         $playerHistory = new $class();
-        $playerHistory->setPlayer( $player );
+        $playerHistory->setPlayer($player);
 
         $entityManager = $this->getEntityManager();
-        $entityManager->persist( $playerHistory );
+        $entityManager->persist($playerHistory);
         $entityManager->flush();
     }
 
@@ -95,8 +95,8 @@ class PlayerHistory extends InvokableBase
         ];
 
         $width = ceil(strlen($text) * $num[$font][0]);
-        $x     = imagesx($img) - $width - 8;
-        $y     = Imagesy($img) - ($num[$font][1] + 2);
-        imagestring($img, $font, $x/2, $y/2, $text, $color);
+        $x = imagesx($img) - $width - 8;
+        $y = Imagesy($img) - ($num[$font][1] + 2);
+        imagestring($img, $font, $x / 2, $y / 2, $text, $color);
     }
 }
