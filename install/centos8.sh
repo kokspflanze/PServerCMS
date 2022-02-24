6db756c2d8e44c5e95fdfbc2eec7899ab64e1094
Oliver <kokspflanze@users.noreply.github.com>
2022-02-24 19:20:37 +0100

    add https support

--- install/centos8.sh
+++ install/centos8.sh
@@ -7,7 +7,7 @@ rpm -Uvh https://rpms.remirepo.net/enterprise/remi-release-8.rpm
 yum -y update
 
 # php && httpd
-yum -y install php74 php74-php php74-php-opcache php74-php-bcmath php74-php-cli php74-php-common php74-php-gd php74-php-intl php74-php-json php74-php-mbstring php74-php-pdo php74-php-pdo-dblib php74-php-pear php74-php-pecl-mcrypt php74-php-xmlrpc php74-php-xml php74-php-mysql php74-php-soap php74-php-pecl-zip php74-php-ioncube-loader httpd firewalld
+yum -y install php74 php74-php php74-php-opcache php74-php-bcmath php74-php-cli php74-php-common php74-php-gd php74-php-intl php74-php-json php74-php-mbstring php74-php-pdo php74-php-pdo-dblib php74-php-pear php74-php-pecl-mcrypt php74-php-xmlrpc php74-php-xml php74-php-mysql php74-php-soap php74-php-pecl-zip php74-php-ioncube-loader httpd firewalld mod_ssl
 
 systemctl start firewalld
 
@@ -44,6 +44,48 @@ ServerSignature Off
 	ErrorLog /var/log/httpd/page-error.log
 	CustomLog /var/log/httpd/page-access.log combined
 </VirtualHost>
+
+
+<VirtualHost _default_:443>
+
+DocumentRoot /var/www/page/public
+<Directory /var/www/page/public>
+		Options -Indexes +FollowSymLinks +MultiViews
+		AllowOverride All
+		Order allow,deny
+		allow from all
+</Directory>
+
+ErrorLog logs/ssl_error_log
+TransferLog logs/ssl_access_log
+LogLevel warn
+
+SSLEngine on
+SSLHonorCipherOrder on
+
+SSLCipherSuite PROFILE=SYSTEM
+SSLProxyCipherSuite PROFILE=SYSTEM
+
+SSLCertificateFile /etc/pki/tls/certs/localhost.crt
+
+SSLCertificateKeyFile /etc/pki/tls/private/localhost.key
+
+<FilesMatch "\.(cgi|shtml|phtml|php)$">
+    SSLOptions +StdEnvVars
+</FilesMatch>
+<Directory "/var/www/cgi-bin">
+    SSLOptions +StdEnvVars
+</Directory>
+
+BrowserMatch "MSIE [2-5]" \
+         nokeepalive ssl-unclean-shutdown \
+         downgrade-1.0 force-response-1.0
+
+CustomLog logs/ssl_request_log \
+          "%t %h %{SSL_PROTOCOL}x %{SSL_CIPHER}x \"%r\" %b"
+
+</VirtualHost>
+
 " > /etc/httpd/conf.d/v-host.conf
 
 mkdir -p /var/www/page/data/DoctrineModule/cache
